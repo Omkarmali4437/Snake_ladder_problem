760ba607fe916e012470f4d294e3251ca72ac957
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-22 14:49:15 +0530

    Added logic for the dice roll

--- snake.java
+++ snake.java
@@ -2,5 +2,7 @@ public class linecomparison {
 	 public static void main(String[] args){
 		System.out.println("Welcome to snake ladder problem");
 		int first_player , start_point=0;
+		first_player=(int)Math.floor((Math.random() * 6) + 1);
+		System.out.println(first_player);
 	}
 }
